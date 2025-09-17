5344d3fdfa02102de75604b9977ed902fd89d793
Maruthi P <maruthip266@gmail.com>
2025-09-18 00:54:56 +0530

    Add files via upload
    printing the patterns

---
+++ pattern03.java
@@ -0,0 +1,12 @@
+package patterns.java;
+
+public class pattern03 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    } 
+}
