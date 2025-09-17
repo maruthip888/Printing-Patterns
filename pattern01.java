b5e76aa4c48d28d74d3a788c94bc31066a7aa587
Maruthi P <maruthip266@gmail.com>
2025-09-18 00:54:14 +0530

    Add files via upload
    printing the different patterns

---
+++ pattern01.java
@@ -0,0 +1,12 @@
+package patterns.java;
+
+public class pattern01 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 2; i++) {
+            for (int j = 0; j <= 4; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ pattern02.java
@@ -0,0 +1,12 @@
+package patterns.java;
+
+public class pattern02 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 4; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
