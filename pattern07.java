b3271745cd54b06d2a9daa8798f8d1b811ea574f
Maruthi P <maruthip266@gmail.com>
2025-09-18 00:57:40 +0530

    Add files via upload
    different patterns are printing in different ways

---
+++ pattern07.java
@@ -0,0 +1,22 @@
+package patterns.java;
+
+public class pattern07 {
+    public static void pattern(int n){
+        for(int i=0; i < n;i++){
+            //Space(n-i-1)
+            for(int j=0; j <n-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int j=0; j<2 * i +1;j++){
+                System.out.print("*");
+            }
+            for(int j=0; j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        pattern(5);
+    }
+}
